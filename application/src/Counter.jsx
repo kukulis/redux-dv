e8376710e0d35d3b4f364f781797784dfa1150bb
Giedrius Tumelis <giedrius.tumelis@regitra.lt>
2025-05-02 14:16:49 +0300

    Experimenting with a redux counter.

--- application/src/Counter.jsx
+++ application/src/Counter.jsx
@@ -1,19 +1,67 @@
-import { useState } from 'react'
+import {useState} from 'react'
+import {configureStore} from "@reduxjs/toolkit";
+
+const counterAddAction = () => {
+    return {
+        type: 'counter/add'
+    }
+}
+
+const counterReducer = (cnt, action) => {
+    if ( cnt === null || cnt === undefined ) {
+        return {count: 13}
+    }
+
+    if (action.type === 'counter/add') {
+        return {...cnt, count: cnt.count + 1}
+    }
+    return cnt;
+};
+
+const store = configureStore({
+    reducer: counterReducer,
+});
+
+console.log('store state at beginning:', store.getState());
+store.dispatch(counterAddAction());
+console.log('store state after dispatch:', store.getState());
+
+const selectCounterCount = (counterState) => counterState.count;
+
 function Counter() {
+
+
     // State: a counter value
     const [counter, setCounter] = useState(0)
 
     // Action: code that causes an update to the state when something happens
     const increment = () => {
         // setCounter(x => x + 1)
-        setCounter(counter+2)
+        setCounter(counter + 2)
     }
+
+    // const handleReduxIncrement = (event) => {
+    //     console.log('handleReduxIncrement called, store state is ', store.getState());
+    //     store.dispatch(counterAddAction());
+    // }
 
     // View: the UI definition
     return (
-        <div>
-            Value: {counter} <button onClick={increment}>Increment</button>
-        </div>
+        <>
+            <div>
+                Value: {counter}
+                <button onClick={increment}>Increment</button>
+            </div>
+            <div>
+                Redux value: {selectCounterCount(store.getState())}
+                <button onClick={(event) => {
+                    event.preventDefault();
+                    console.log('onClick called, store state is ', store.getState());
+                    store.dispatch(counterAddAction());
+                }}>Increment redux
+                </button>
+            </div>
+        </>
     )
 }
 
