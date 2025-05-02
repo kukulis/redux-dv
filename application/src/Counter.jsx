461c02984acd1377d588a4bcf69fd5f1a884fecb
Giedrius Tumelis <giedrius.tumelis@regitra.lt>
2025-05-02 11:11:00 +0300

    Primitive example from the fundamentals start.

---
+++ application/src/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+function Counter() {
+    // State: a counter value
+    const [counter, setCounter] = useState(0)
+
+    // Action: code that causes an update to the state when something happens
+    const increment = () => {
+        setCounter(x => x + 1)
+    }
+
+    // View: the UI definition
+    return (
+        <div>
+            Value: {counter} <button onClick={increment}>Increment</button>
+        </div>
+    )
+}
+
+export default Counter;
